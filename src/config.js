8feba2bd081174d1df52fb2ab54dfaa2626c6c11
Steven Thornton <stevenwthornton0@gmail.com>
2019-10-12 14:54:08 -0500

    fixing for deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT_FOLDERS: 'http://localhost:8000/folders',
-  API_ENDPOINT_NOTES: 'http://localhost:8000/notes'
+  API_ENDPOINT_FOLDERS: 'https://pacific-river-64951.herokuapp.com/folders',
+  API_ENDPOINT_NOTES: 'https://pacific-river-64951.herokuapp.com/notes'
 }
